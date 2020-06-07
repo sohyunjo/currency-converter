3a9d41b1e811a1f9979cdadd70d61d2de0b09ea2
sohyunjo <sohyunjo9@gmail.com>
2020-06-06 22:37:02 -0400

    done

--- currency-converter.js
+++ currency-converter.js
@@ -12,15 +12,32 @@ require("dotenv").config();
 //     });
 // };
 
-
 const getExchangeRate = async (fromCurrency, toCurrency) => {
-    const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${process.env.CURRENCY_KEY}&format=1`)
-    
+    const response = await axios.get(
+        `http://data.fixer.io/api/latest?access_key=${process.env.CURRENCY_KEY}&format=1`
+    );
+
     const rate = response.data.rates;
-    const euro = 1 / rate[fromCurrency]
-    const exchangeRate = euro * rate[toCurrency]
+    const from = 1 / rate[fromCurrency];
+    const exchangeRate = from * rate[toCurrency];
 
-    console.log(exchangeRate)
-}
+    return exchangeRate;
+};
 
-getExchangeRate('USD', 'EUR')
+const getCountries = async (toCurrency) => {
+    const response = await axios.get(
+        `http://restcountries.eu/rest/v2/currency/${toCurrency}`
+    );
+
+    return response.data.map((country) => console.log(country.name));
+};
+
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+    const countries = await getCountries(toCurrency);
+    const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+};
+
+convertCurrency("USD", "EUR", 30).then((message) => console.log(message));
