1bf4a2c6c7f551268a8d8b8d38759f3cb0c86af3
sohyunjo <sohyunjo9@gmail.com>
2020-06-06 22:49:04 -0400

    added error catch

--- currency-converter.js
+++ currency-converter.js
@@ -21,23 +21,37 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
     const from = 1 / rate[fromCurrency];
     const exchangeRate = from * rate[toCurrency];
 
+    if (isNaN(exchangeRate)) {
+        throw new Error(
+            `Unable to get currency ${fromCurrency} and ${toCurrency}`
+        );
+    }
+
     return exchangeRate;
 };
 
 const getCountries = async (toCurrency) => {
-    const response = await axios.get(
-        `http://restcountries.eu/rest/v2/currency/${toCurrency}`
-    );
-
-    return response.data.map((country) => console.log(country.name));
+    try{
+        const response = await axios.get(
+            `http://restcountries.eu/rest/v2/currency/${toCurrency}`
+        );
+    
+        return response.data.map((country) => console.log(country.name));
+    }catch(error) {
+        throw new Error(`Unable to get countries that use ${toCurrency}`)
+    }
+    
 };
 
 const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
     const countries = await getCountries(toCurrency);
+    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+
     const convertedAmount = (amount * exchangeRate).toFixed(2);
 
     return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
 };
 
-convertCurrency("USD", "EUR", 30).then((message) => console.log(message));
+convertCurrency("USD", "HAR", 30)
+    .then((message) => console.log(message))
+    .catch((err) => console.log(err));
